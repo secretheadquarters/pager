498bbbcd58650b43b06da68e0ce1f86814c02ccd
secretheadquarters <54038940+secretheadquarters@users.noreply.github.com>
2019-09-20 00:49:43 +0100

    Remove unnecessary interactive checks in setup script

--- setup/setup.sh
+++ setup/setup.sh
@@ -4,12 +4,7 @@ echo Update the password for the current user
 passwd
 
 CURRENT_HOSTNAME=`cat /etc/hostname | tr -d " \t\n\r"`
-if [ "$INTERACTIVE" = True ]; then
-  NEW_HOSTNAME=$(whiptail --inputbox "Please enter a hostname" 20 60 "$CURRENT_HOSTNAME" 3>&1 1>&2 2>&3)
-else
-  NEW_HOSTNAME=$1
-  true
-fi
+NEW_HOSTNAME=$(whiptail --inputbox "Please enter a hostname" 20 60 "$CURRENT_HOSTNAME" 3>&1 1>&2 2>&3)
 if [ $? -eq 0 ]; then
   echo $NEW_HOSTNAME > /etc/hostname
   sed -i "s/127.0.1.1.*$CURRENT_HOSTNAME/127.0.1.1\t$NEW_HOSTNAME/g" /etc/hosts
