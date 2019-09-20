778b038e0aa6bd95c64ddf908d402faf38e1f5a3
secretheadquarters <54038940+secretheadquarters@users.noreply.github.com>
2019-09-20 01:40:26 +0100

    Update setup.sh to only use sudo when necessary

--- setup/setup.sh
+++ setup/setup.sh
@@ -6,12 +6,15 @@ passwd
 CURRENT_HOSTNAME=`cat /etc/hostname | tr -d " \t\n\r"`
 NEW_HOSTNAME=$(whiptail --inputbox "Please enter a hostname" 20 60 "$CURRENT_HOSTNAME" 3>&1 1>&2 2>&3)
 if [ $? -eq 0 ]; then
-  echo $NEW_HOSTNAME > /etc/hostname
-  sed -i "s/127.0.1.1.*$CURRENT_HOSTNAME/127.0.1.1\t$NEW_HOSTNAME/g" /etc/hosts
+  echo $NEW_HOSTNAME | sudo tee /etc/hostname
+  sudo sed -i "s/127.0.1.1.*$CURRENT_HOSTNAME/127.0.1.1\t$NEW_HOSTNAME/g" /etc/hosts
 fi
 
-apt-get update
+sudo apt-get update
+sudo apt-get --yes install git
 
-raspi-config --expand-rootfs
+git clone https://github.com/secretheadquarters/pager.git
+
+sudo raspi-config --expand-rootfs
 
 whiptail --msgbox "Now please reboot the system" 20 60 2
